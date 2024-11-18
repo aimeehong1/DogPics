2a7b692df372fc2128c5eca78b474c786678b3b6
Aimee Hong <honglp@bc.edu>
2024-11-18 11:37:27 -0500

    Initial Commit

---
+++ DogPics/DogPicsApp.swift
@@ -0,0 +1,17 @@
+//
+//  DogPicsApp.swift
+//  DogPics
+//
+//  Created by Aimee Hong on 11/18/24.
+//
+
+import SwiftUI
+
+@main
+struct DogPicsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
